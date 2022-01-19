c9c70134b7282156725246d2d249ab43549e2f6c
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-19 14:53:52 +0900

    Create yarn.sh

---
+++ packages/yarn.sh
@@ -0,0 +1,11 @@
+
+sudo apt remove cmdtest -y
+sudo apt remove yarn -y
+# Add apt-key from yarnpkg
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# Add repository from yarnpkg
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# Update => Reload /etc/apt/{sources.list,sources.list.d}
+sudo apt-get update -y
+# Install yarn
+sudo apt-get install yarn -y
