cc4ab55c88ca99fdec5afcd55760c95bcbac9585
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-19 15:30:49 +0900

    Fix permission => linuxbrew

--- packages/brew.sh
+++ packages/brew.sh
@@ -14,13 +14,19 @@ sudo -u linuxbrew /bin/bash -c \
         # Without wait_for_user
         sed 's/wait_for_user()/wait_four__user\(\)/' | \
         sed 's/wait_for_user//')"
-sudo chmod -R 755 /home/linuxbrew/.linuxbrew/
 # Set brew binary path to /etc/profile.d/brew_bin_path.sh
 echo 'Set brew binary path to /etc/profile.d/brew_bin_path.sh'
 echo 'export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin' | sudo tee /etc/profile.d/brew_bin_path.sh
+# Set permission to readable,executable
+echo 'Set permission to readable,executable to profile'
+sudo chmod 755 /etc/profile.d/brew_bin_path.sh
+# Set permission rwxrwxrwx to linuxbrew
+echo 'Set permission rwxrwxrwx to linuxbrew'
+sudo chmod 777 -R /home/linuxbrew/.linuxbrew/
 # Set brew binary path
 echo 'Set brew binary path'
 export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+
 # Remove user linuxbrew from sudoers list
 echo 'Remove user linuxbrew from sudoers list'
 sudo rm -rf /etc/sudoers.d/linuxbrew-install
