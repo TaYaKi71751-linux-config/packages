9b50a5b6ba2fbfc963f0a75953551c7c17ccae4f
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-19 11:58:29 +0900

    Create neovim.sh

---
+++ packages/neovim.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Install neovim
+sudo apt-get install neovim -y
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Download ".config/nvim/init.vim"
+download_file(){
+    FILE_PATH="$1"
+    echo "Download $FILE_PATH"
+    mkdir "~/$FILE_PATH"
+    rm -rf "~/$FILE_PATH"
+    curl -LsSf "https://raw.githubusercontent.com/raccl/packages/ubuntu/$FILE_PATH" -o - | tee "$HOME/$FILE_PATH"
+}
+download_file ".config/nvim/init.vim"
+
+# Install Plugins
+vim +PlugInstall +qall
