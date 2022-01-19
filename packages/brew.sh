a9baaf5d101d73f4575edf6d7d2f0e7391ee80b7
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-19 16:13:16 +0900

    Add sudo

--- packages/brew.sh
+++ packages/brew.sh
@@ -2,21 +2,22 @@
 # 
 # Add user linuxbrew
 echo 'Add user linuxbrew'
-useradd -mG linuxbrew linuxbrew
+sudo useradd -mG linuxbrew linuxbrew
 # Add user linuxbrew to sudoers list
 echo 'Add user linuxbrew to sudoers list'
-echo '%linuxbrew ALL=(ALL:ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/linuxbrew-install
+echo 'linuxbrew ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/linuxbrew-install
 # Install linuxbrew to user linuxbrew
 echo 'Install linuxbrew to user linuxbrew'
-sudo -u linuxbrew /bin/bash -c \
-    "$(curl -fsSL \
-            https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | \
-        # Without wait_for_user
-        sed 's/wait_for_user()/wait_four__user\(\)/' | \
-        sed 's/wait_for_user//')"
+
+BREW_INSTALL=`curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh" | \
+     sed 's/wait_for_user()/wait_four__user()/' | \
+      sed 's/wait_for_user//'` # Without wait_for_user
+echo "${BREW_INSTALL}" | tee /tmp/BREW_INSTALL.sh
+sudo -u linuxbrew /bin/bash /tmp/BREW_INSTALL.sh
+
 # Set brew binary path to /etc/profile.d/brew_bin_path.sh
 echo 'Set brew binary path to /etc/profile.d/brew_bin_path.sh'
-echo 'export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin' | sudo tee /etc/profile.d/brew_bin_path.sh
+echo 'export PATH="${PATH}:/home/linuxbrew/.linuxbrew/bin"' | sudo tee /etc/profile.d/brew_bin_path.sh
 # Set permission to readable,executable
 echo 'Set permission to readable,executable to profile'
 sudo chmod 755 /etc/profile.d/brew_bin_path.sh
@@ -25,7 +26,7 @@ echo 'Set permission rwxrwxrwx to linuxbrew'
 sudo chmod 777 -R /home/linuxbrew/.linuxbrew/
 # Set brew binary path
 echo 'Set brew binary path'
-export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+export PATH=${PATH}:/home/linuxbrew/.linuxbrew/bin
 
 # Remove user linuxbrew from sudoers list
 echo 'Remove user linuxbrew from sudoers list'
