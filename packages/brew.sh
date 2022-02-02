0ea5f9111779ba3ca80311e11b0f27d1320f6a9c
TaYaKi71751 <shantian.tw@gmail.com>
2022-02-02 21:39:10 +0900

    Add install git to brew.sh

--- packages/brew.sh
+++ packages/brew.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+ORG='raccl'
+REPO='packages'
+BRANCH='ubuntu'
+RAW_DIR='packages'
+install_pkg(){
+    PKG="$1"
+    echo "Install $PKG"
+    sh -c "$(curl -LsSf https://raw.githubusercontent.com/${ORG}/${REPO}/${BRANCH}/${RAW_DIR}/${PKG}.sh)"
+}
+install_pkg "git"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh |\
 	sed -e 's/unset\ HAVE_SUDO_ACCESS/HAVE_SUDO_ACCESS="false"/g' |\
