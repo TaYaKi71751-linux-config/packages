9f35668d2691c744fca41f987b20fb1baaa0cd4f
TaYaKi71751 <shantian.tw@gmail.com>
2022-02-23 03:44:45 +0000

    Use apt_install

--- packages/tailscale.sh
+++ packages/tailscale.sh
@@ -4,5 +4,30 @@ no_pw_sudo(){
  local CMD="$@"
  printf "\n\n\n" | sudo -S $CMD || echo "ERROR: No permissions to no_pw_sudo"
 }
-# Install tailscale script
-no_pw_sudo "curl -fsSL https://tailscale.com/install.sh | sh"
+apt_install(){
+ local PKG="$@"
+ no_pw_sudo "apt-get update -y"
+ no_pw_sudo "apt-get install ${PKG} -y"
+}
+
+which apt-add-repository || apt_install "software-properties-common"
+which apt-key || apt_install "apt-key"
+
+OS=$(cat /etc/lsb-release | grep ID | rev | cut -d '=' -f1 | rev | tr '[:upper:]' '[:lower:]')
+VERSION=$(cat /etc/lsb-release | grep CODENAME | rev | cut -d '=' -f1 | rev)
+
+# Add keyring
+printf "\n\n\n" |\
+	sudo -S sh -c "curl -LsSf \
+		https://pkgs.tailscale.com/stable/${OS}/${VERSION}.asc |\
+			sudo apt-key add -"
+
+# Add repository
+printf "\n\n\n" |\
+	sudo -S sh -c "curl -fsSL \
+		https://pkgs.tailscale.com/stable/${OS}/${VERSION}.list |\
+			sudo tee /etc/apt/sources.list.d/tailscale.list"
+
+# Install tailscale
+apt_install "tailscale"
+
